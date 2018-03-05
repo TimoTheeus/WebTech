986d337a1d3ca87c8a92112d2cf9fb372fcb0c8c
Timo <cobravspanda01@gmail.com>
2018-03-05 17:00:59 +0100

    contextmenu

--- script.js
+++ script.js
@@ -14,3 +14,42 @@ $(function() {
         $('article').before($toc);
     }
 });
+$(document).ready(function(){
+
+    //When clicking anywhere on the page that is not the contextmenu, hide the custom contextmenu
+    $(document).click(function(){
+            $(document.getElementsByClassName("customContextMenu")).addClass("hidden").removeClass("show");
+    });
+    //when right clicking on a piece of text in an article
+    $("article p, article h1, article h2").on('contextmenu', e => {
+        //prevent the default context menu
+        e.preventDefault();
+        //show the contextmenu
+        $(document.getElementsByClassName("customContextMenu")).addClass("show").removeClass("hidden");
+        $(document.getElementsByClassName("customContextMenu")).css('top',e.pageY);
+        $(document.getElementsByClassName("customContextMenu")).css('left',e.pageX);
+        $("#bold").click(function(){
+            $(e.target).css("font-weight","bold");
+        });
+        $("#unBold").click(function(){
+            $(e.target).css("font-weight","normal");
+        });
+        $("#italic").click(function(){
+            $(e.target).css("font-style","italic");
+        });
+        $("#unItalic").click(function(){
+            $(e.target).css("font-style","normal");
+        });
+        $("#changeBlack").click(function(){
+            $(e.target).css("color",'black');
+        });
+        $("#changeGrey").click(function(){
+            $(e.target).css("color",'#696969');
+        });
+        $("#changeBrown").click(function(){
+            $(e.target).css("color",'brown');
+        });
+    });
+});
+
+
