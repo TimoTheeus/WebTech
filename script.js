21c301c0f613137787128c2081b296841e7b2bde
Vogel100 <fabbimaggi@hotmail.com>
2018-03-05 16:31:40 +0100

    Finished the graph more or less, I think
    Also shortened some code.

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ $(function() {
         var $tocList = $('<ol>');
         $toc.append($tocList);
         $h2s.each(function() {
-            var txt = this.innerHTML;
-            var id = txt.replace(/ /g, '_');
-            this.id = id;
-            $tocList.append(`<li><a href="#${id}">${txt}</a></li>`);
+            this.id = this.innerHTML.replace(/ /g, '_');
+            $tocList.append(`<li><a href="#${this.id}">${this.innerHTML}</a></li>`);
         });
         $('article').before($toc);
     }
-    new Dygraph(document.getElementById('graph'), 'Fennec_vs_Red.csv', {colors: ['yellow', '#b75800']});
+    if (typeof Dygraph == 'function')
+        new Dygraph(document.getElementById('graph'), 'Fennec_vs_Red.csv', {
+            colors: ['chocolate', 'red'],
+            clickCallback: function(_, x) {
+                var curRange = this.xAxisRange();
+                var newRange = (curRange[1] - curRange[0]) / ((Math.abs($('#zoom').val()) || 2) * 2);
+                this.updateOptions({dateWindow: [x - newRange, x + newRange]});
+            }
+    });
 });
